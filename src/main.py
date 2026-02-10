0b0d3751bdfecb24fd0ba1615fe605730dfec277
homostultus39 <homostultus39@gmail.com>
2026-02-10 23:03:13 +0300

    Убрал неиспольуземые участи кода

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends
-from alembic import command
-from alembic.config import Config
 
 from src.management.logger import configure_logger
 from src.api.v1.peers.router import router as peers_router
@@ -11,30 +8,10 @@ from src.api.v1.deps.middlewares.auth import get_current_api_key
 
 logger = configure_logger("MAIN", "cyan")
 
-def run_migrations():
-    logger.info("Running migrations...")
-    alembic_cfg = Config("alembic.ini")
-    command.upgrade(alembic_cfg, "head")
-    logger.info("Migrations applied successfully.")
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    run_migrations()
-    logger.info("Migrations applied successfully.")
-    logger.info("Creating default protocols...")
-    # await create_default_protocols()
-    logger.info("Default protocols created successfully.")
-    logger.info("Creating default API key...")
-    # await create_default_api_key()
-    logger.info("Default API key created successfully.")
-    logger.info("Initialization completed successfully.")
-    yield
 
 app = FastAPI(
     title="Amnezia API",
     version="1.0.0",
-    lifespan=lifespan,
     root_path="/api/v1",
     swagger_ui_parameters={"persistAuthorization": True},
 )
